905834312d73360d884c64da51825e40acf9a2f3
Mirellys Arteta Davila <martetadavila@anwb.nl>
2018-06-03 00:46:53 +0200

    remove navigation from main activity

---
+++ app/src/main/java/com/syllerim/restaurants/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.syllerim.restaurants.activity
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import com.syllerim.restaurants.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
